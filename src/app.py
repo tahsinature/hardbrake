c37308299b17f2c69f01baea88cc1d280c78c1fa
Tahsin <tahsingenuies@gmail.com>
2023-02-17 16:04:09 +0700

    move some func to utils

--- src/app.py
+++ src/app.py
@@ -2,17 +2,14 @@ from rich import traceback
 
 import file
 import prompts
-import handbrake
 import manager
-
+import utils
 
 traceback.install()
 
+utils.verify_installation("HandBrakeCLI")
 
-hb = handbrake.Instance()
-categories = hb.get_presets()
-
-
+categories = utils.get_presets()
 category = prompts.multi_select(choices=list(categories.keys()))
 presets = categories[category]
 preset = prompts.multi_select(choices=presets)
@@ -23,11 +20,5 @@ preset = prompts.multi_select(choices=presets)
 #          '/Users/tahsin/Desktop/hb-play/sample-15s.mp4', '/Users/tahsin/Desktop/hb-play/sample-20s.mp4', '/Users/tahsin/Desktop/hb-play/sample-30s.mp4']
 
 files = file.select_files()
-cmds = [handbrake.get_cmd(file_path, preset) for file_path in files]
+cmds = [utils.get_encode_cmd(file_path, preset) for file_path in files]
 manager.manage(cmds)
-
-
-# for file_path in files:
-#     hb.encode(file_path, preset)
-
-# hb.encode(files[-1], "Android 480p30")
