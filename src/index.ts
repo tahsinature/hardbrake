df6b5a9d78da8db6dafb8e8a9cb07da72d000d24
Tahsin <work.tahsin@gmail.com>
2024-11-10 17:40:05 -0500

    add github ci for bun

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,31 @@ import { askFiles, askPreset, askToggle } from "./prompts";
 import { main } from "./engine";
 import { checkRequiredBinaries } from "./check";
 
-await checkRequiredBinaries();
+const hardbrake = async () => {
+  await checkRequiredBinaries();
 
-const files = await askFiles();
-if (files.length === 0) {
-  console.log("No files selected. Exiting.");
-  process.exit(0);
-}
+  const files = await askFiles();
+  if (files.length === 0) {
+    console.log("No files selected. Exiting.");
+    process.exit(0);
+  }
 
-const preset = await askPreset();
+  const preset = await askPreset();
 
-await main(files, preset);
+  await main(files, preset);
 
-const happyWithResults = await askToggle("Are you happy with the results?");
-if (!happyWithResults) {
-  for (const file of files) await file.deleteOutput();
-  console.log("Deleted all the output files.");
-  process.exit(0);
-}
+  const happyWithResults = await askToggle("Are you happy with the results?");
+  if (!happyWithResults) {
+    for (const file of files) await file.deleteOutput();
+    console.log("Deleted all the output files.");
+    process.exit(0);
+  }
 
-const deleteOriginalFiles = await askToggle("Do you want to delete the original files?");
-if (deleteOriginalFiles) {
-  for (const file of files) await file.deleteOriginal();
-  console.log("Deleted all the original files.");
-}
+  const deleteOriginalFiles = await askToggle("Do you want to delete the original files?");
+  if (deleteOriginalFiles) {
+    for (const file of files) await file.deleteOriginal();
+    console.log("Deleted all the original files.");
+  }
+};
+
+export default hardbrake;
