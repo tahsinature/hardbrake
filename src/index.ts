e9d43f1f719a4e2a6b95922f3cd053899014c382
Tahsin <work.tahsin@gmail.com>
2024-11-19 18:39:11 -0500

    setup default value for askToggle

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,18 @@ const hardbrake = async () => {
   }
 
   const preset = await askPreset();
-  const keepAudio = await askToggle("Do you want to keep the audio?");
+  const keepAudio = await askToggle("Do you want to keep the audio?", { initial: true });
 
   await main(files, preset, { keepAudio });
 
-  const happyWithResults = await askToggle("Are you happy with the results?");
+  const happyWithResults = await askToggle("Are you happy with the results?", { initial: true });
   if (!happyWithResults) {
     for (const file of files) await file.deleteOutput();
     console.log("Deleted all the output files.");
     process.exit(0);
   }
 
-  const deleteOriginalFiles = await askToggle("Do you want to delete the original files?");
+  const deleteOriginalFiles = await askToggle("Do you want to delete the original files?", { initial: false });
   if (deleteOriginalFiles) {
     for (const file of files) await file.deleteOriginal();
     console.log("Deleted all the original files.");
