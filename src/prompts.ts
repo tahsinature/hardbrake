ea8d0037245145ef47e52641240296147adb1ccf
Tahsin <work.tahsin@gmail.com>
2024-12-14 23:01:25 -0500

    replace all prompts using gum

--- src/prompts.ts
+++ src/prompts.ts
@@ -1,69 +1,88 @@
-import prompts from "prompts";
 import path from "path";
-import { getPresets, runShellCommand } from "./utils";
+import fs from "fs";
+import { getPresets, runShellCommandAndReturnOutput } from "./utils";
 import { File } from "./blueprints";
 
-function promptsWithOnCancel<T extends string = string>(questions: prompts.PromptObject<T> | Array<prompts.PromptObject<T>>) {
-  return prompts(questions, {
-    onCancel: () => {
-      process.exit(0);
-    },
-  });
-}
+export const askFiles = async (supportedExtensions: string[]) => {
+  const dir = await askFolderPath();
+  const files = fs.readdirSync(dir);
+  const videos = files.filter((file) => supportedExtensions.includes(file.split(".").pop() || ""));
+  if (videos.length === 0) throw new Error("No files found in the folder");
+  const selectedVids = await askFilter("Choose videos", videos, { min: 1 });
+  if (selectedVids.length === 0) throw new Error("No files selected");
 
-export const askFiles = async () => {
-  const supportedExt = ["mp4", "mkv", "avi", "mov", "flv", "wmv", "webm", "m4v", "lrf"];
-  const extentionsFlags = supportedExt.map((ext) => `-e ${ext}`).join(" ");
-  const command = `fd ${extentionsFlags} -t f --max-depth 1 --absolute-path . | fzf --multi --bind ctrl-space:toggle-all`;
-
-  const files = await runShellCommand(command);
-  if (!files) return [];
-
-  const filtered = files.split("\n").filter(Boolean);
-  return filtered.map((file) => path.resolve(file)).map((file) => new File(file));
+  return selectedVids.map((file) => new File(path.join(dir, file)));
 };
 
-export const askAutoComplete = async (message: string, choices: string[]) => {
-  const response = await promptsWithOnCancel({
-    type: "autocomplete",
-    name: "value",
-    message,
-    choices: choices.map((choice) => ({ title: choice, value: choice })),
-  });
+export const askFolderPath = async () => {
+  console.log("Select a directory or, a file. If a file is selected, the parent directory will be used.");
 
-  return response.value;
+  const command = `gum file --all --directory .`;
+  const result = runShellCommandAndReturnOutput(command);
+  if (result.length === 0) throw new Error("Nothing selected");
+  let dirPath = result[0];
+
+  const isDir = fs.lstatSync(dirPath).isDirectory();
+  if (!isDir) dirPath = path.dirname(dirPath);
+  return dirPath;
+};
+
+export const askChoose = async (message: string, choices: string[], { limit = 1 }: { limit?: number } = {}) => {
+  try {
+    const gumChoices = choices.join(",");
+    let command = `gum choose {${gumChoices}}`;
+
+    if (limit) command += ` --limit ${limit}`;
+    else command += " --no-limit";
+
+    return runShellCommandAndReturnOutput(command);
+  } catch (error: any) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+export const askFilter = async (message: string, choices: string[], { limit = 0, min = 0 }: { limit?: number; min?: number } = {}) => {
+  try {
+    const gumChoices = choices.join("\n");
+    let command = `echo "${gumChoices}" | gum filter`;
+
+    if (limit) command += ` --limit ${limit}`;
+    else command += " --no-limit";
+
+    const selected = runShellCommandAndReturnOutput(command);
+    if (selected.length < min) throw new Error("Minimum number of choices not selected");
+    return selected;
+  } catch (error: any) {
+    console.error(error.message);
+    process.exit(1);
+  }
 };
 
 const fullPresetList = await getPresets();
 const categories = Object.keys(fullPresetList);
 
 export const askPreset = async () => {
-  const category = await askAutoComplete("Select a category", categories);
-  const presets = fullPresetList[category];
-  const preset = await askAutoComplete("Select a preset", presets);
+  const category = await askFilter("Select a category", categories, { limit: 1, min: 1 });
+  const presets = fullPresetList[category[0]];
+  const preset = await askFilter("Select a preset", presets, { limit: 1, min: 1 });
 
-  return preset;
+  return preset[0];
 };
 
-export const askBoolean = async (message: string) => {
-  const response = await promptsWithOnCancel({
-    type: "confirm",
-    name: "answer",
-    message,
-  });
-
-  return response.answer;
+export const askBoolean = async (message: string, initial?: "true" | "false") => {
+  const defaultFlag = initial ? `--default=${initial}` : "";
+  const command = `gum confirm ${defaultFlag} "${message}" && echo "true" || echo "false"`;
+  const response = runShellCommandAndReturnOutput(command);
+  return response[0] === "true";
 };
 
-export const askToggle = async (message: string, { initial = false } = {}) => {
-  const response = await promptsWithOnCancel({
-    type: "toggle",
-    name: "value",
-    message,
-    initial,
-    active: "yes",
-    inactive: "no",
-  });
+export const askInteger = async (message: string, initial?: number) => {
+  const initialFlag = initial ? `--value=${initial}` : "";
+  const command = `gum input --placeholder="${message}" ${initialFlag}`;
+  const response = runShellCommandAndReturnOutput(command);
+  const nm = parseInt(response[0]);
+  if (isNaN(nm)) throw new Error("Invalid number");
 
-  return response.value;
+  return nm;
 };
